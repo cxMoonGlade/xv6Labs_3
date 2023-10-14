5cbe0df9604285fc8c9206458c3c7cad6ed8830c
cxMoonGlade <920977575@qq.com>
2023-10-13 19:52:00 -0700

    add 2 syscall functions to syscall.c One is sys_trace The other one is sys_sysinfo

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -104,6 +104,8 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_wait(void);
 extern uint64 sys_write(void);
 extern uint64 sys_uptime(void);
+extern uint64 sys_trace(void); // Part A for Lab2
+extern uint64 sys_sysinfo(void); // Part B for Lab2
 
 static uint64 (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -127,6 +129,8 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_trace]   sys_trace,
+[SYS_sysinfo] sys_sysinfo,
 };
 
 void
